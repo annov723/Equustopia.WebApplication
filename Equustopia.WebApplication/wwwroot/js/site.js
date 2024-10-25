18014f096a295738254aabd10b4f2dd05119c2b1
annov723 <annov723@gmail.com>
2024-10-25 23:24:35 +0200

    header is ready

--- Equustopia.WebApplication/wwwroot/js/site.js
+++ Equustopia.WebApplication/wwwroot/js/site.js
@@ -1,4 +1,13 @@
-﻿ function toggleSideMenu() {
-    const sideMenu = document.querySelector('.side-menu');
-    sideMenu.classList.toggle('open');
-}
+﻿const menuToggle = document.getElementById('side-menu-toggle');
+const sideMenu = document.getElementById('side-menu');
+
+menuToggle.addEventListener('click', function(e){
+   sideMenu.classList.toggle('open');
+   e.stopPropagation();
+});
+
+document.addEventListener('click', function(e){
+    if(sideMenu.classList.contains('open') && !sideMenu.contains(e.target) && !menuToggle.contains(e.target)){
+        sideMenu.classList.remove('open');
+    }
+});
