b29752edcefb7fc56f6f88efe18d51d99129fd64
annov723 <annov723@gmail.com>
2024-11-05 19:29:19 +0100

    login UI ready

--- Equustopia.WebApplication/wwwroot/js/login.js
+++ Equustopia.WebApplication/wwwroot/js/login.js
@@ -2,14 +2,9 @@
     document.getElementById("loginModal").style.display = "block";
 }
 
-function closeModal() {
-    document.getElementById("loginModal").style.display = "none";
-}
-
-// Close modal when clicking outside of it
-window.onclick = function(event) {
-    var modal = document.getElementById("loginModal");
-    if (event.target === modal) {
-        closeModal();
-    }
-};
+// window.onclick = function(event) {
+//     var modal = document.getElementById("loginModal");
+//     if (event.target === modal) {
+//         closeModal();
+//     }
+// };
