ea514fe056c4cedc49fed26ef1695f115d3e7f16
annov723 <124592563+annov723@users.noreply.github.com>
2025-01-01 21:33:06 +0100

    Pr5 basic logging logic (#6)

---
+++ database/equustopia.sql
@@ -0,0 +1,169 @@
+CREATE SCHEMA main;
+CREATE SCHEMA analytics;
+
+CREATE TABLE main."userData"(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(60) NOT NULL
+);
+CREATE TABLE main."equestrianCentre"(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(250) NOT NULL,
+    "userId" INTEGER NOT NULL,
+    latitude DOUBLE PRECISION,
+    longitude DOUBLE PRECISION,
+    address VARCHAR(250),
+    UNIQUE (latitude, longitude),
+    FOREIGN KEY ("userId") REFERENCES main."userData"(id) ON DELETE CASCADE
+);
+CREATE TABLE main.horse(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "centreId" INTEGER,
+    FOREIGN KEY ("userId") REFERENCES main."userData"(id) ON DELETE CASCADE,
+    FOREIGN KEY ("centreId") REFERENCES main."equestrianCentre"(id) ON DELETE SET NULL
+);
+
+CREATE TABLE analytics."pagesViews"(
+    id SERIAL PRIMARY KEY,
+    "userId" INTEGER,
+    "pageId" INTEGER NOT NULL,
+    "pageType" VARCHAR(50) CHECK ("pageType" IN ('equestrianCentre', 'horse')) NOT NULL,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    "ipAddress" VARCHAR(45),
+    FOREIGN KEY ("userId") REFERENCES main."userData"(id)
+);
+
+CREATE VIEW analytics."mostViewedPages" AS
+    SELECT "pageId", "pageType",
+           COUNT(*) AS "viewsCount",
+           RANK() OVER (ORDER BY COUNT(*) DESC) AS Pozycja
+FROM analytics."pagesViews"
+GROUP BY "pageId", "pageType"
+ORDER BY "viewsCount" DESC
+LIMIT 10;
+
+INSERT INTO main."userData" (name, email, password)
+VALUES ('User2', 'user2@gmail.com', 'haslohaslo');
+
+SELECT * FROM main."userData";
+
+ALTER TABLE main."userData" ADD CONSTRAINT chk_name_length CHECK (LENGTH(name) BETWEEN 2 AND 50);
+ALTER TABLE main."userData" ADD CONSTRAINT chk_email_length CHECK (LENGTH(email) BETWEEN 5 AND 255);
+ALTER TABLE main."userData" ADD CONSTRAINT chk_password_length CHECK (LENGTH(password) BETWEEN 4 AND 60);
+
+INSERT INTO main.horse (name, "userId") VALUES ('Blobiczek', 36);
+INSERT INTO main.horse (name, "userId") VALUES ('Juglaś', 36);
+
+ALTER TABLE main.horse ADD COLUMN "birthDate" DATE;
+
+ALTER TABLE main.horse ADD CONSTRAINT chk_name_length CHECK (LENGTH(name) BETWEEN 2 AND 50);
+
+
+--15.11.2024
+INSERT INTO main."equestrianCentre" (name, "userId") VALUES ('Jazda Konna Blobi', 32);
+
+--19.11.2024
+ALTER TABLE main."equestrianCentre" ADD CONSTRAINT chk_name_length CHECK (LENGTH(name) BETWEEN 2 AND 250);
+
+--20.11.2024
+--podział konisiów na wiek
+SELECT name, "userId",
+        CASE
+            WHEN "birthDate" < (CURRENT_DATE - INTERVAL '15 years') THEN 'gen X'
+            WHEN "birthDate" > (CURRENT_DATE - INTERVAL '15 years') AND "birthDate" < (CURRENT_DATE - INTERVAL '5 years') THEN 'millennials'
+            WHEN "birthDate" > (CURRENT_DATE - INTERVAL '5 years') THEN 'gen Z'
+            ELSE 'no birthdate'
+        END
+FROM main.horse;
+
+--liczenie ile w danej stajni jest koni o danym wieku
+SELECT e.name,
+       SUM(CASE WHEN h."birthDate" < (CURRENT_DATE - INTERVAL '15 years') THEN 1 ELSE 0 END) as genX,
+       SUM(CASE WHEN h."birthDate" > (CURRENT_DATE - INTERVAL '15 years') AND h."birthDate" < (CURRENT_DATE - INTERVAL '5 years') THEN 1 ELSE 0 END) as millennials,
+       SUM(CASE WHEN h."birthDate" > (CURRENT_DATE - INTERVAL '5 years') THEN 1 ELSE 0 END) as genZ,
+       SUM(CASE WHEN h."birthDate" is NULL THEN 1 ELSE 0 END) as empty
+FROM main.horse h JOIN main."equestrianCentre" e on h."centreId" = e.id
+GROUP BY e.name, h."centreId"
+ORDER BY e.name;
+
+--lista użytkowników, którzy mają konia starszego niż 10 lat jeśli mieszka w ośrodku 2
+--konia starszego niż 5 lat jeśli mieszka w ośrodku 5
+--konia starszego niż 1 rok jeśli mieszka w ośrodku 1
+SELECT u.email, u.name, h.name, h."birthDate"
+FROM main."userData" u JOIN main.horse h on u.id = h."userId"
+WHERE h."birthDate" <
+CASE
+    WHEN h."centreId" IN (2) THEN (CURRENT_DATE - INTERVAL '10 years')
+    WHEN h."centreId" IN (5) THEN (CURRENT_DATE - INTERVAL '5 years')
+    WHEN h."centreId" IN (1) THEN (CURRENT_DATE - INTERVAL '1 years')
+END
+ORDER BY 4 DESC;
+
+--ośrodki które mają więcej niż 1 konia
+WITH statementCTE AS (SELECT "centreId", COUNT(*) AS num FROM main.horse GROUP BY "centreId")
+SELECT e.name FROM statementCTE JOIN main."equestrianCentre" e on "centreId" = e.id WHERE num >= 2;
+
+--27.11.2024
+CREATE OR REPLACE FUNCTION "horsesInCentre"(centreId INT)
+RETURNS TABLE(name VARCHAR, birthDate DATE, userId INT) AS
+$$
+    SELECT h.name, h."birthDate", h."userId"
+    FROM main.horse h
+    WHERE h."centreId" = centreId;
+$$ LANGUAGE sql;
+SELECT "horsesInCentre"(5);
+
+CREATE OR REPLACE FUNCTION "userName" (int)
+RETURNS text AS
+$$
+    DECLARE
+        "idUser" ALIAS FOR $1;
+        namee main."userData".name%TYPE;
+    BEGIN
+        SELECT INTO namee name FROM main."userData"
+        WHERE id = "idUser";
+        RETURN namee;
+    END;
+$$ LANGUAGE 'plpgsql';
+SELECT "userName"(1);
+
+CREATE OR REPLACE FUNCTION "userNameAndEmail" (int)
+RETURNS text AS
+$$
+    DECLARE
+        "idUser" ALIAS FOR $1;
+        namee RECORD;
+    BEGIN
+        SELECT name, email INTO namee FROM main."userData"
+        WHERE id = "idUser";
+        RETURN namee.name || ' ' || namee.email;
+    END;
+$$ LANGUAGE 'plpgsql';
+SELECT "userNameAndEmail"(1);
+
+DO $$
+    BEGIN
+        RAISE SQLSTATE '00200'; --wysłanie kodu błędu
+END $$;
+
+CREATE OR REPLACE FUNCTION "showUserWithError" ("userId" int) RETURNS text AS $$
+DECLARE
+    owner main."userData"%ROWTYPE;
+BEGIN
+    SELECT INTO owner * FROM main."userData" WHERE id = "userId";
+    IF NOT FOUND THEN
+        RAISE EXCEPTION 'No owner in base!';
+    END IF;
+    RETURN owner.name || ' ' || owner.email;
+END;
+$$
+LANGUAGE 'plpgsql';
+SELECT "showUserWithError"(1);
+
+--29.11.2024
+ALTER TABLE analytics."pagesViews" ADD CONSTRAINT chk_page_type_length CHECK (LENGTH("pageType") BETWEEN 2 AND 50);
+ALTER TABLE analytics."pagesViews" ADD CONSTRAINT chk_ip_address_length CHECK (LENGTH("ipAddress") BETWEEN 2 AND 45);
+
